e873b58d3d7be5fbeea01352fedf4c31f85a6c82
vivek chouhan <vivekchouha1n@gmail.com>
2026-06-19 17:46:33 +0530

    package

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Vercel serverless entrypoint: export the Express app instance.
-// The @vercel/node builder will call this as the function handler.
-const app = require('../app');
-
-module.exports = app;
